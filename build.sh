898e28906e74c1151c039836a42418bb03e44335
Teja Allada <tejavenkatarajuallada@gmail.com>
2026-03-31 11:47:22 +0530

    Fix requirements for Render deployment

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 # exit on error
 set -o errexit
 
-pip install -r requirementss.txt
-pip install gunicorn whitenoise
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
